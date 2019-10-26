afe6222db5b7356f37750197286873fda37e9078
vispacem <47271346+vispacem@users.noreply.github.com>
2019-10-26 16:03:33 +0200

    Update foreground.sh

--- ipynb_test/foreground.sh
+++ ipynb_test/foreground.sh
@@ -1,5 +1,5 @@
 sudo git clone https://github.com/LordGhostX/IPYNB-viewer.git
 cd IPYNB-viewer
-pip3 install --update -r requirements.txt
+pip3 install -r requirements.txt
 wget https://www.southampton.ac.uk/~fangohr/teaching/python/book/ipynb/01-introduction.ipynb
-viewer.py 01-introduction.ipynb
+python3 viewer.py 01-introduction.ipynb
