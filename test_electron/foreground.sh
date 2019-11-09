56bc66b1b690d77a7b9e071b83678199c8600b37
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-09 12:06:58 +0100

    Update foreground.sh

--- test_electron/foreground.sh
+++ test_electron/foreground.sh
@@ -1,17 +1,10 @@
 sudo git clone https://github.com/resyu/Scratchpad.git
 cd Scratchpad/
 
-apt-get purge wine*
-yes | apt-get autoremove 
-rm -rf .wine
-dpkg --add-architecture i386
-wget -nc https://dl.winehq.org/wine-builds/winehq.key
-apt-key add winehq.key
-apt-add-repository https://dl.winehq.org/wine-builds/debian/
-apt-get install -y software-properties-common
-apt-add-repository https://dl.winehq.org/wine-builds/debian/
+apt-get update && apt-get install -y --no-install-recommends software-properties-common && dpkg --add-architecture i386 && curl -L https://dl.winehq.org/wine-builds/winehq.key > winehq.key && apt-key add winehq.key && apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
 apt-get update
-apt-get install -y apt-transport-https
-apt-get update
-apt-get install -y --install-recommends winehq-stable
-wine notepad.exe
+apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
+apt-get install -y --no-install-recommends winehq-stable
+# clean
+apt-get clean && rm -rf /var/lib/apt/lists/* && unlink winehq.key
+
