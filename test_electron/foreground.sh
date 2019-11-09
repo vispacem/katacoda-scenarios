d11609a4dd635ebca854f141aea088fcfe9a373b
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-09 13:30:05 +0100

    Update foreground.sh

--- test_electron/foreground.sh
+++ test_electron/foreground.sh
@@ -8,3 +8,6 @@ apt-get install python-software-properties software-properties-common libicns-de
 apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
 apt-get update
 apt-get install --install-recommends winehq-staging wine-gecko -y
+curl -L https://github.com/electron-userland/electron-builder-binaries/releases/download/wine-2.0.3-mac-10.13/wine-home.zip > /tmp/wine-home.zip && unzip /tmp/wine-home.zip -d /root/.wine && unlink /tmp/wine-home.zip
+
+
