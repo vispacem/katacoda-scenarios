58f3d5a0c5bc6cc768c3d5fa399d58f82f7fbc1d
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-09 12:51:17 +0100

    Update foreground.sh

--- test_electron/foreground.sh
+++ test_electron/foreground.sh
@@ -1,10 +1,10 @@
 sudo git clone https://github.com/resyu/Scratchpad.git
 cd Scratchpad/
-
-apt-get update && apt-get install -y --no-install-recommends software-properties-common && dpkg --add-architecture i386 && curl -L https://dl.winehq.org/wine-builds/winehq.key > winehq.key && apt-key add winehq.key && apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
+dpkg --add-architecture i386
+wget https://dl.winehq.org/wine-builds/Release.key
+apt-key add Release.key
 apt-get update
-apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
-apt-get install -y --no-install-recommends winehq-stable
-# clean
-apt-get clean && rm -rf /var/lib/apt/lists/* && unlink winehq.key
-
+apt-get install python-software-properties software-properties-common libicns-dev libicns1 -y
+apt-add-repository 'https://dl.winehq.org/wine-builds/ubuntu/'
+apt-get update
+apt-get install --install-recommends winehq-staging wine-gecko -y
