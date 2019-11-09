a9ca95f6b3e37c1a5a7728e616ccdd42800b9ed6
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-09 17:10:54 +0100

    Update foreground.sh

--- test_electron/foreground.sh
+++ test_electron/foreground.sh
@@ -1,5 +1,4 @@
-# sudo git clone https://github.com/resyu/Scratchpad.git
-# cd Scratchpad/
+# install wine
 apt-get update && apt-get install -y --no-install-recommends software-properties-common && dpkg --add-architecture i386 && curl -L https://dl.winehq.org/wine-builds/winehq.key > winehq.key && apt-key add winehq.key && apt-add-repository https://dl.winehq.org/wine-builds/ubuntu && \
   apt-get update && \
   apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties && \
@@ -8,4 +7,13 @@ apt-get update && apt-get install -y --no-install-recommends software-properties
   apt-get clean && rm -rf /var/lib/apt/lists/* && unlink winehq.key
 
 curl -L https://github.com/electron-userland/electron-builder-binaries/releases/download/wine-2.0.3-mac-10.13/wine-home.zip > /tmp/wine-home.zip && unzip /tmp/wine-home.zip -d /root/.wine && unlink /tmp/wine-home.zip
-
+# clone Scratchpad source
+git clone https://github.com/vispacem/Scratchpad.git
+cd Scratchpad/
+npm install
+npm run dist
+cd build/
+# send to ffsend
+wget https://github.com/timvisee/ffsend/releases/download/v0.2.55/ffsend-v0.2.55-linux-x64-static
+chmod 777 ff*
+./ffsend* upload scr* -h https://stage.send.nonprod.cloudops.mozgcp.net/
