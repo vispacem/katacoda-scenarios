00b2cd7c5240e13bfc1c2e8943c61c38855221de
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-09 11:55:31 +0100

    Create foreground.sh

---
+++ test_electron/foreground.sh
@@ -0,0 +1,15 @@
+sudo git clone https://github.com/resyu/Scratchpad.git
+cd Scratchpad/
+
+apt-get purge wine* && apt-get autoremove && rm -rf .wine
+dpkg --add-architecture i386
+wget -nc https://dl.winehq.org/wine-builds/winehq.key
+apt-key add winehq.key
+apt-add-repository https://dl.winehq.org/wine-builds/debian/
+apt-get install -y software-properties-common
+apt-add-repository https://dl.winehq.org/wine-builds/debian/
+apt-get update
+apt-get install -y apt-transport-https
+apt-get update
+apt-get install -y --install-recommends winehq-stable
+wine notepad.exe
