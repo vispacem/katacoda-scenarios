99bee625a0ace1706da5f33bc9355e8329d56c7e
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 13:47:30 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -10,12 +10,16 @@ apt-get -y install --install-recommends winehq-stable
 apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
 sudo apt-get -y install --no-install-recommends --allow-unauthenticated winehq-stable fakeroot libxss1
 # get source
-git clone https://github.com/vispacem/Scratchpad.git
-cd Scratchpad/
-npm install
-npm run dist
+
+git clone https://github.com/zone1240/electron-vue-app-demo.git
+cd electron-vue-app-demo/
+yarn install
+yarn run build:win32
 cd build/
+tar -cjvf archive.tar.bz2 electron-vue-app-demo-win32-x64
+
+
 # send to ffsend
 wget https://github.com/timvisee/ffsend/releases/download/v0.2.55/ffsend-v0.2.55-linux-x64-static
 chmod 777 ff*
-./ffsend* upload scr* -h https://stage.send.nonprod.cloudops.mozgcp.net/
+./ffsend* upload archive* -h https://stage.send.nonprod.cloudops.mozgcp.net/
