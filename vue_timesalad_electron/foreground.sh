5975dcb17aba8d5c20ca4208d4c0a65714ea3458
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 12:08:50 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -1,19 +1,9 @@
-# install wine
-apt-get update
-apt-get install -y --no-install-recommends software-properties-common
-dpkg --add-architecture i386
-curl -L https://dl.winehq.org/wine-builds/winehq.key > winehq.key
-apt-key add winehq.key
-apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
-apt-get update
-apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
-apt-get install -y --no-install-recommends winehq-stable fakeroot libxss1
-# clean
-sudo apt-get clean
-sudo rm -rf /var/lib/apt/lists/*
-sudo unlink winehq.key
-curl -L https://github.com/electron-userland/electron-builder-binaries/releases/download/wine-2.0.3-mac-10.13/wine-home.zip > /tmp/wine-home.zip && unzip /tmp/wine-home.zip -d /root/.wine && unlink /tmp/wine-home.zip
-# clone Scratchpad source
+sudo dpkg --add-architecture i386
+wget -nc dl.winehq.org/wine-builds/Release.key
+sudo apt-key add Release.key
+sudo apt-add-repository dl.winehq.org/wine-builds/ubuntu/
+sudo apt-get update
+sudo apt-get install --install-recommends winehq-stable
 git clone https://github.com/vispacem/TIMESALAD.git
 cd TIMESALAD/
 npm install
