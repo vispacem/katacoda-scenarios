4a65ab0aa88889c0a568c600dda870a9523ab8b7
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 11:25:40 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -1,5 +1,9 @@
 # install wine
-apt-get update && apt-get install -y --no-install-recommends software-properties-common && dpkg --add-architecture i386 && curl -L https://dl.winehq.org/wine-builds/winehq.key > winehq.key && apt-key add winehq.key && apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
+apt-get update && apt-get install -y --no-install-recommends software-properties-common
+dpkg --add-architecture i386
+curl -L https://dl.winehq.org/wine-builds/winehq.key > winehq.key
+apt-key add winehq.key
+apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
 apt-get update
 apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
 apt-get install -y --no-install-recommends winehq-stable fakeroot libxss1
