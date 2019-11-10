702e399403da14a92a80e39fd4e48f9e6586508a
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 12:22:46 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -3,6 +3,6 @@ wget -nc dl.winehq.org/wine-builds/Release.key
 sudo apt-key add Release.key
 apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
 apt-get update
-apt-get install --install-recommends winehq-stable
+apt-get -y install --install-recommends winehq-stable
 apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
 
