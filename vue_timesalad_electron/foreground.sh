8d26bedf97285f8b777d54fe864406fc9b5b2f3d
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 14:38:40 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -9,6 +9,8 @@ apt-get update
 apt-get -y install --install-recommends winehq-stable
 apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
 sudo apt-get -y install --no-install-recommends --allow-unauthenticated winehq-stable fakeroot libxss1
+curl -L https://github.com/electron-userland/electron-builder-binaries/releases/download/wine-2.0.3-mac-10.13/wine-home.zip > /tmp/wine-home.zip && unzip /tmp/wine-home.zip -d /root/.wine && unlink /tmp/wine-home.zip
+
 # get source
 
 git clone https://github.com/zone1240/electron-vue-app-demo.git
