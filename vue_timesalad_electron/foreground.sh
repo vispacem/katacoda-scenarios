b4a1970139b4c3605c1a4c62fba967a5063c7ab9
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 12:21:29 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -1,16 +1,8 @@
 sudo dpkg --add-architecture i386
 wget -nc dl.winehq.org/wine-builds/Release.key
 sudo apt-key add Release.key
-sudo apt-add-repository dl.winehq.org/wine-builds/ubuntu/
-sudo apt-get update
-sudo apt-get install --install-recommends winehq-stable
-git clone https://github.com/vispacem/TIMESALAD.git
-cd TIMESALAD/
-npm install
-# unsupported platform for fsevents@1.2.4: wanted {"os":"darwin","arch":"any"} (current: {"os":"linux","arch":"x64"})
-npm run build
-cd build/
-# send to ffsend
-wget https://github.com/timvisee/ffsend/releases/download/v0.2.55/ffsend-v0.2.55-linux-x64-static
-chmod 777 ff*
-./ffsend* upload scr* -h https://stage.send.nonprod.cloudops.mozgcp.net/
+apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
+apt-get update
+apt-get install --install-recommends winehq-stable
+apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
+
