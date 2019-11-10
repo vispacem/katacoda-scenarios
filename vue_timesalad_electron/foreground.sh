c1c0e24b7cd79cd39ef805e194bd09dfd66c0fae
vispacem <47271346+vispacem@users.noreply.github.com>
2019-11-10 12:37:32 +0100

    Update foreground.sh

--- vue_timesalad_electron/foreground.sh
+++ vue_timesalad_electron/foreground.sh
@@ -5,7 +5,7 @@ apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
 apt-get update
 apt-get -y install --install-recommends winehq-stable
 apt-get -y purge software-properties-common libdbus-glib-1-2 python3-dbus python3-gi python3-pycurl python3-software-properties
-apt-get -y install --no-install-recommends --allow-unauthenticated winehq-stable fakeroot libxss1
+sudo apt-get -y install --no-install-recommends --allow-unauthenticated winehq-stable fakeroot libxss1
 # get source
 git clone https://github.com/vispacem/Scratchpad.git
 cd Scratchpad/
